3286e01007c8385b773ac04bbc3a2a9068ddfb4c
Dyeni9 <dyenifersilva9@gmail.com>
2025-08-31 15:49:48 -0400

    final

--- chefinho-cv/Chefinho_cv_javascript/script.js
+++ chefinho-cv/Chefinho_cv_javascript/script.js
@@ -1,6 +1,4 @@
-// Espera o conteúdo da página ser totalmente carregado
 window.addEventListener('DOMContentLoaded', (event) => {
-    // Encontra o nosso container e adiciona a classe para iniciar a animação
     const container = document.querySelector('.curriculo-container');
     container.classList.add('pagina-visivel');
 });
